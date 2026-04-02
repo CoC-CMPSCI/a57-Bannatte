e5d0b2233061f687835c7b3f07950f2e81843483
Bannatte <32626837+Bannatte@users.noreply.github.com>
2026-04-01 18:39:20 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,24 @@ int main()
 	cin >> n1 >> n2;
 	cin >> N;
 
-	// TODO
-	// TODO
+	for (int i = 0; i < N; ++i)
+	{
+		cout << n1 << endl;
+		n1, n2 = n2, n1 + n2
+	}
+	
+	return 0;
 }
+
+
+/*
+Assignment Directions:
+
+Write a program that generates a sequence of numbers where each number is the sum of the previous two numbers.
+Ask for the first two initial values.
+Ask for the input N for the number of sequences (N > 2).
+Print all numbers in the sequence of summation values.
+For example, for inputs 5, 8, 3: the number of sequences is 3 and the initial two values are 5 and 8. Print 3 numbers: 5 8 13 (13 is the sum of the previous two numbers).
+Another example: inputs 5 10 5 → output: 5 10 15 25 40
+
+*/
